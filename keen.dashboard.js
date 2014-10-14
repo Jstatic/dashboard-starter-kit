792c0465f3571ffd6b858bb36b034351de7266da
Jason Vertrees <jason.vertrees@realmassive.com>
2014-10-14 16:54:45 -0500

    [WIP] - playing around w/customization

--- keen.dashboard.js
+++ keen.dashboard.js
@@ -1,24 +1,43 @@
 var client = new Keen({
-  projectId: "5368fa5436bf5a5623000000",
-  readKey: "3f324dcb5636316d6865ab0ebbbbc725224c7f8f3e8899c7733439965d6d4a2c7f13bf7765458790bd50ec76b4361687f51cf626314585dc246bb51aeb455c0a1dd6ce77a993d9c953c5fc554d1d3530ca5d17bdc6d1333ef3d8146a990c79435bb2c7d936f259a22647a75407921056"
+  projectId: "529f7b3ece5e431cfe000010",
+  readKey: "83740031e2adc8dca547fe9abf6afa9bdbe1d52959425c915590d291d6bb28a40c11283a508de78cf7d5bd8f68017d8dedea883010ef33287324c16dc624e351b132a2022e16d8f5d58fb9c826c05c21089fdfda8f084918cc04e723435e3d120cb1cfecbf4f57302545e3c9419caaf6"
 });
 
 Keen.ready(function(){
 
-
   // ----------------------------------------
-  // Pageviews Area Chart
+  // Pageviews Area Chart (30 days)
   // ----------------------------------------
-  var pageviews_timeline = new Keen.Query("count", {
-    eventCollection: "pageviews",
-    interval: "hourly",
-    groupBy: "user.device_info.browser.family",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
+  var spaceviews_timeline_30d = new Keen.Query("count", {
+    eventCollection: "space_views",
+    interval: "daily",
+    timeframe: "previous_30_day"
+  });
+  client.draw(spaceviews_timeline_30d, document.getElementById("spaceviews-30d"), {
+    chartType: "areachart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "80%"
+      },
+      isStacked: true
     }
   });
-  client.draw(pageviews_timeline, document.getElementById("chart-01"), {
+
+  // ----------------------------------------
+  // Portfolio Additions (30d)
+  // ----------------------------------------
+  var port_add_timeline_30d = new Keen.Query("count", {
+    eventCollection: "adding_space_to_portfolio",
+    interval: "daily",
+    timeframe: "previous_30_days"
+  });
+  client.draw(port_add_timeline_30d, document.getElementById("portfolioadds-30d"), {
     chartType: "areachart",
     title: false,
     height: 250,
@@ -36,17 +55,86 @@ Keen.ready(function(){
 
 
   // ----------------------------------------
-  // Pageviews Pie Chart
+  // Pageviews Area Chart (24 hrs)
   // ----------------------------------------
-  var pageviews_static = new Keen.Query("count", {
-    eventCollection: "pageviews",
-    groupBy: "user.device_info.browser.family",
-    timeframe: {
-      start: "2014-05-01T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
+  var spaceviews_timeline_24h = new Keen.Query("count", {
+    eventCollection: "space_views",
+    interval: "hourly",
+    timeframe: "previous_24_hours"
+  });
+  client.draw(spaceviews_timeline_24h, document.getElementById("spaceviews-24h"), {
+    chartType: "areachart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "80%"
+      },
+      isStacked: true
     }
   });
-  client.draw(pageviews_static, document.getElementById("chart-02"), {
+
+  // ----------------------------------------
+  // Portfolio Additions (24h)
+  // ----------------------------------------
+  var port_add_timeline_24h = new Keen.Query("count", {
+    eventCollection: "adding_space_to_portfolio",
+    interval: "hourly",
+    timeframe: "previous_24_hours"
+  });
+  client.draw(port_add_timeline_24h, document.getElementById("portfolioadds-24h"), {
+    chartType: "areachart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "80%"
+      },
+      isStacked: true
+    }
+  });
+
+
+  // ----------------------------------------
+  // Pageviews Pie Chart (24hrs)
+  // ----------------------------------------
+  var spaceviews_pie_24h = new Keen.Query("count", {
+    eventCollection: "space_views",
+    groupBy: "space_id",
+    timeframe: "previous_24_hours"
+  });
+  client.draw(spaceviews_pie_24h, document.getElementById("spaceviews-by-space-24h"), {
+    chartType: "piechart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "100%"
+      }
+    }
+  });
+
+  // ----------------------------------------
+  // Pageviews Pie Chart (24hrs)
+  // ----------------------------------------
+  var spaceviews_pie_24h = new Keen.Query("count", {
+    eventCollection: "space_views",
+    groupBy: "user_id",
+    timeframe: "previous_24_hours"
+  });
+  client.draw(spaceviews_pie_24h, document.getElementById("spaceviews-by-user-24h"), {
     chartType: "piechart",
     title: false,
     height: 250,
@@ -62,100 +150,55 @@ Keen.ready(function(){
   });
 
 
+
+
   // ----------------------------------------
-  // Impressions timeline
+  // Spaces added to portfolio by Space
   // ----------------------------------------
-  var impressions_timeline = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "ad.advertiser",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
+  var port_add_pie = new Keen.Query("count", {
+    eventCollection: "adding_space_to_portfolio",
+    timeframe: "previous_24_hours",
+    groupBy: "space_id"
   });
-  client.draw(impressions_timeline, document.getElementById("chart-03"), {
-    chartType: "columnchart",
+  client.draw(port_add_pie, document.getElementById("portfolioadds-by-space-24hs"), {
+    chartType: "piechart",
     title: false,
     height: 250,
     width: "auto",
     chartOptions: {
       chartArea: {
-        height: "75%",
-        left: "10%",
+        height: "85%",
+        left: "5%",
         top: "5%",
-        width: "60%"
-      },
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
+        width: "100%"
+      }
     }
   });
 
-
   // ----------------------------------------
-  // Impressions timeline (device)
+  // Spaces added to portfolio by User
   // ----------------------------------------
-  var impressions_timeline_by_device = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "user.device_info.device.family",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
+  var port_add_pie = new Keen.Query("count", {
+    eventCollection: "adding_space_to_portfolio",
+    timeframe: "previous_24_hours",
+    groupBy: "user_id"
   });
-  client.draw(impressions_timeline_by_device, document.getElementById("chart-04"), {
-    chartType: "columnchart",
+  client.draw(port_add_pie, document.getElementById("portfolioadds-by-user-24hs"), {
+    chartType: "piechart",
     title: false,
     height: 250,
     width: "auto",
     chartOptions: {
       chartArea: {
-        height: "75%",
-        left: "10%",
+        height: "85%",
+        left: "5%",
         top: "5%",
-        width: "60%"
-      },
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
+        width: "100%"
+      }
     }
   });
 
 
-  // ----------------------------------------
-  // Impressions timeline (country)
-  // ----------------------------------------
-  var impressions_timeline_by_country = new Keen.Query("count", {
-    eventCollection: "impressions",
-    groupBy: "user.geo_info.country",
-    interval: "hourly",
-    timeframe: {
-      start: "2014-05-04T00:00:00.000Z",
-      end: "2014-05-05T00:00:00.000Z"
-    }
-  });
-  client.draw(impressions_timeline_by_country, document.getElementById("chart-05"), {
-    chartType: "columnchart",
-    title: false,
-    height: 250,
-    width: "auto",
-    chartOptions: {
-      chartArea: {
-        height: "75%",
-        left: "10%",
-        top: "5%",
-        width: "60%"
-      },
-      bar: {
-        groupWidth: "85%"
-      },
-      isStacked: true
-    }
-  });
 
 
 });
